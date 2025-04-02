3af5e8d13b953855dc09f3b37e7e0b6985845cc1
Marina <marinamagedsamuel@gmail.com>
2025-04-02 12:07:25 -0700

    Code Refactor

--- NasrallaM_HW3/Nasralla_geometry.js
+++ NasrallaM_HW3/Nasralla_geometry.js
@@ -1,2 +1,53 @@
 // Marina Nasralla
 // 4/9/25
+
+points = []
+indices = []
+normals = []
+
+// f is a function
+function generatePoints(f, a, b, stepT, stepTheta){
+    points = []
+    normals = []
+    if(a > b){
+        temp = a
+        a = b
+        b = temp
+    }
+
+    // build object from bottom to top
+    // the first "stepTheta" points build the bottom most circle (at t = -1)
+    for(t = a; t <= b; t += stepT){
+        for(theta = 0; theta <= radians(360); theta += stepTheta){
+            // rotate about y
+            // we know that z is 0 in f(t) so we'll just use the x component
+            x = f(t) * Math.cos(theta)
+            y = t
+            z = f(t) * -Math.sin(theta)
+            points.push(vec3(x,y,z))
+            normals.push(vec3(x,y,z))
+        }
+    }
+}
+
+// triangulation
+function generateIndices(stepTheta){
+    indices = []
+    numSteps = radians(360) / stepTheta
+    // .
+    // |\
+    // .-.
+    for(i = 0; i < points.length - numSteps; i++){
+        indices.push(i)
+        indices.push(i+numSteps)
+        indices.push(i+numSteps+1)
+    }
+    //   .
+    //  /|
+    // .-.
+    for(i = 0; i < points.length - numSteps-1; i++){
+        indices.push(i)
+        indices.push(i+1)
+        indices.push(i+1+numSteps)
+    }
+}
--- NasrallaM_HW3/Nasralla_hw3.js
+++ NasrallaM_HW3/Nasralla_hw3.js
@@ -1,10 +1,5 @@
 // Marina Nasralla
 // 4/9/25
-
-points = []
-indices = []
-normals = []
-
 var program
 var viewer = 
 {
@@ -45,51 +40,6 @@ var ambientColor, diffuseColor, specularColor;
 
 function SOR(x){return Math.sin(x) + (1/2)*Math.sin(6*x)}
 function Cylinder(x){return 1}
-
-// f is a function
-function generatePoints(f, a, b, stepT, stepTheta){
-    points = []
-    if(a > b){
-        temp = a
-        a = b
-        b = temp
-    }
-
-    // build object from bottom to top
-    // the first "stepTheta" points build the bottom most circle (at t = -1)
-    for(t = a; t <= b; t += stepT){
-        for(theta = 0; theta <= radians(360); theta += stepTheta){
-            // rotate about y
-            // we know that z is 0 in f(t) so we'll just use the x component
-            x = f(t) * Math.cos(theta)
-            y = t
-            z = f(t) * -Math.sin(theta)
-            points.push(vec3(x,y,z))
-            normals.push(vec3(x,y,z))
-        }
-    }
-}
-
-// triangulation
-function generateIndices(stepTheta){
-    numSteps = radians(360) / stepTheta
-    // .
-    // |\
-    // .-.
-    for(i = 0; i < points.length - numSteps; i++){
-        indices.push(i)
-        indices.push(i+numSteps)
-        indices.push(i+numSteps+1)
-    }
-    //   .
-    //  /|
-    // .-.
-    for(i = 0; i < points.length - numSteps-1; i++){
-        indices.push(i)
-        indices.push(i+1)
-        indices.push(i+1+numSteps)
-    }
-}
 
 window.onload = function init() {
     canvas = document.getElementById( "gl-canvas" );
